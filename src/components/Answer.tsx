840b1860fbde59c110eec4057b470ca1b8df60cd
Breno Lemos <brenomiros@gmail.com>
2022-12-15 21:09:32 -0300

    Answer back style

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -15,28 +15,31 @@ const Answer = (props: AnswerProps) => {
   return (
     <div className={styles.answer} onClick={() => props.onAnswer(props.index)}>
       <div className={styles["answer__content"]}>
-        {/* <div className={styles.front}>
-          <div
-            className={styles.option}
-            style={{ backgroundColor: props.optionColorBg }}
-          >
-            {props.option}
+        {!answer.revealed ? (
+          <div className={styles.front}>
+            <div
+              className={styles.option}
+              style={{ backgroundColor: props.optionColorBg }}
+            >
+              {props.option}
+            </div>
+            <div className={styles.value}>{answer.value}</div>
           </div>
-          <div className={styles.value}>{answer.value}</div>
-        </div> */}
-        <div className={styles.back}>
-          {answer.correct ? (
-            <div className={styles.correct}>
-              <div>The correct answer is...</div>
-              <div className={styles.value}>{answer.value}</div>
-            </div>
-          ) : (
-            <div className={styles.incorrect}>
-              <div>The answer selected is incorrect...</div>
-              <div className={styles.value}>{answer.value}</div>
-            </div>
-          )}
-        </div>
+        ) : (
+          <div className={styles.back}>
+            {answer.correct ? (
+              <div className={styles.correct}>
+                <div>The correct answer is...</div>
+                <div className={styles.value}>{answer.value}</div>
+              </div>
+            ) : (
+              <div className={styles.incorrect}>
+                <div>The answer selected is incorrect...</div>
+                <div className={styles.value}>{answer.value}</div>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
