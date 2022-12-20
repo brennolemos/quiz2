21afe18c0d6aaa8a5fbd154516d838bc6b74c4c8
Breno Lemos <brenomiros@gmail.com>
2022-12-20 18:35:07 -0300

    Add the loadQuestion methods

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import AnswerModel from "model/Answer";
 import QuestionModel from "model/Question";
 import Quiz from "components/Quiz";
+
+const BASE_URL = "http://localhost:3000/api";
 
 const mockQuestion = new QuestionModel(1, "Melhor cor?", [
   AnswerModel.incorrect("Green"),
@@ -12,28 +14,38 @@ const mockQuestion = new QuestionModel(1, "Melhor cor?", [
 ]);
 
 export default function Home() {
+  const [questionsIds, setQuestionsIds] = useState([]);
   const [question, setQuestion] = useState(mockQuestion);
+
+  const loadQuestionsIds = async () => {
+    const resp = await fetch(`${BASE_URL}/quiz`);
+    const questionsIds = await resp.json();
+    setQuestionsIds(questionsIds);
+  };
+
+  const loadQuestion = async (questionId: number) => {
+    const resp = await fetch(`${BASE_URL}/questions/${questionId}`);
+    const json = await resp.json();
+  };
+
+  useEffect(() => {
+    loadQuestionsIds();
+  }, []);
+
+  useEffect(() => {
+    questionsIds.length && loadQuestion(questionsIds[0]);
+  }, [questionsIds]);
 
   const responseQuestion = (question: QuestionModel) => {};
 
   const goToNext = () => {};
 
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "100vh",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Quiz
-        question={question}
-        isLast={true}
-        responseQuestion={responseQuestion}
-        goToNext={goToNext}
-      />
-    </div>
+    <Quiz
+      question={question}
+      isLast={true}
+      responseQuestion={responseQuestion}
+      goToNext={goToNext}
+    />
   );
 }
